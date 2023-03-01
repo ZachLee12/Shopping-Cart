bbd2b485a2bad72f18c35b0a733aeb0023ad066e
ZachLee12 <leezhengyang22@gmail.com>
2023-03-01 22:43:48 +0100

    ShopItem now renders as a child component of Shop

--- src/Routes/Shop/Shop.jsx
+++ src/Routes/Shop/Shop.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import ShopItem from "../ShopItem/ShopItem";
+import PlaceholderImage from '../../assets/images/placeholderImage.jpg'
+import uniqid from 'uniqid';
+
 
 export default function Shop() {
+
+    const initialState = {
+        itemList: [{
+            image: PlaceholderImage,
+            name: 'Sample Item',
+            id: uniqid(),
+            price: '19.99 CHF',
+            addedToCart: false,
+            isFavourite: false,
+            description: 'This is a sample description about the ShopItem.'
+        }],
+    }
+
+    const [shop, setShop] = React.useState(initialState)
 
     return (
         <div id='Shop'>
             <h1>Shop page</h1>
 
-            <ShopItem />
+
+            {shop.itemList.map(item => {
+                return (
+                    <ShopItem />
+                )
+            })}
+            {/* <ShopItem /> */}
         </div>
     )
 }
--- src/Routes/ShopItem/ShopItem.jsx
+++ src/Routes/ShopItem/ShopItem.jsx
@@ -3,27 +3,33 @@ import uniqid from 'uniqid';
 import PlaceholderImage from '../../assets/images/placeholderImage.jpg'
 
 export default function ShopItem(props) {
-    const initialState = {
-        image: PlaceholderImage,
-        name: 'Sample Item',
-        id: uniqid(),
-        description: 'This is a sample description about the ShopItem.'
-    }
+    // const initialState = {
+    //     image: PlaceholderImage,
+    //     name: 'Sample Item',
+    //     id: uniqid(),
+    //     addedToCart: false,
+    //     isFavourite: false,
+    //     description: 'This is a sample description about the ShopItem.'
+    // }
 
-    const [item, setItem] = React.useState(initialState);
-
+    // const [item, setItem] = React.useState();
 
 
     return (
         <React.Fragment>
             <figure className="ShopItem">
-                <img className="ShopItem-image" src={PlaceholderImage} alt="" />
+                <img className="ShopItem-image" src={props.image} alt="" />
                 <figcaption>
-                    <p className="ShopItem-name">{item.name}</p>
-                    <p className="ShopItem-description">{item.description}</p>
+                    <p className="ShopItem-name">{props.name}</p>
+                    <p className="ShopItem-description">{props.description}</p>
+                    <p className="price">19.99 CHF</p>
+                    <p className="favourite-star" >⭐ ☆</p>
+                    <button onClick={() => setItem({
+                        ...props,
+                        addedToCart: !item.addedToCart
+                    })}>{props.addedToCart ? '✔ Added to Cart' : 'Add to Cart'}</button>
                 </figcaption>
             </figure>
-
         </React.Fragment>
     )
 }
