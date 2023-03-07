bdb640412853e848fe594d3dbcadf9871e475d87
ZachLee12 <leezhengyang22@gmail.com>
2023-03-07 16:32:45 +0100

    Style basic Checkout Component

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -8,22 +8,28 @@ export default function Checkout({ itemList, renderCheckout, handleClickUnmountC
 
     return (
         <React.Fragment>
-            {renderCheckout &&
+            
 
-                <div id='Checkout'>
-                    <h1>CHECKOUT</h1>
-                    {itemList.map(item =>
-                        <div key={item.id}>
-                            <p>{item.name}</p>
-                            <img src={item} alt="" />
+                <div id="Checkout-fullscreen-wrapper" className={renderCheckout ? '' : 'Checkout-wrapper-slide-out'}>
+                    <div id="Checkout-wrapper">
+                        <div id='Checkout'>
+                            <h1 className="checkout-title">CHECKOUT</h1>
+                            <div className="checkout-items-wrapper">
+                                {itemList.map(item =>
+                                    <div key={item.id} className='checkout-item'>
+                                        <p>{item.name}</p>
+                                        <img src={item.image} alt="" />
+                                    </div>
+                                )}
+                            </div>
+                            <button
+                                onClick={handleClickUnmountCheckout}
+                                className="buy-now-button">
+                                Buy now!
+                            </button>
                         </div>
-                    )}
-                    <button
-                        onClick={handleClickUnmountCheckout}
-                        className="buy-now-button">
-                        Buy now!
-                    </button>
-                </div>}
+                    </div>
+                </div>
         </React.Fragment>
     )
 }
