cb906a06f6395fe35d8598caf6c12b3fec2d88c0
ZachLee12 <leezhengyang22@gmail.com>
2023-03-14 16:38:40 +0100

    Add liquid effect to shop-image

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,18 +1,20 @@
 import React from "react"
+import PlaceholderImage from '../../assets/images/placeholderImage.jpg'
 
 export default function MainPage({ shouldMount }) {
     return (
         <React.Fragment>
-            {shouldMount && <div id='MainPage'>
-                <div className="shop-introduction-container">
-                    <p className='shop-title'>Main Shop</p>
-                    <p className='shop-description'>Some other description about this main shop</p>
-                </div>
-                <div className="vertical-line"></div>
-                <div className='image-positioning-container'>
-                    <div className="image-container"></div>
-                </div>
-            </div>}
+            {shouldMount &&
+                <div id='MainPage'>
+                    <div className="shop-introduction-container">
+                        <p className='shop-title'>Main Shop</p>
+                        <p className='shop-description'>Some other description about this main shop</p>
+                    </div>
+                    <div className="vertical-line"></div>
+
+                    <img src={PlaceholderImage} className='shop-image' />
+
+                </div>}
         </React.Fragment>
     )
 
