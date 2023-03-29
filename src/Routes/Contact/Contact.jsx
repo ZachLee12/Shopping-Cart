1ca4bbc36dc0cf29c55d82f79b6d25bc2a76e144
ZachLee12 <leezhengyang22@gmail.com>
2023-03-29 13:08:50 +0200

    Style layout of ContactPage

--- src/Routes/Contact/Contact.jsx
+++ src/Routes/Contact/Contact.jsx
@@ -3,8 +3,13 @@ import React from "react";
 export default function Contact() {
 
     return (
-        <div>
-            <h1>Contact Page</h1>
+        <div id="Contact">
+            <section className="contact-content">
+                <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore ab cumque id, consequatur voluptatum, voluptatibus magnam unde expedita deserunt ipsa in. Eaque vero deleniti eligendi esse, odit sed nemo nisi.
+                    Dolorum provident pariatur, quas, nihil dolor, commodi consequatur adipisci non ab asperiores harum minus? Eligendi ut distinctio, totam alias doloribus cum voluptatem ipsum velit a incidunt commodi cumque minus omnis.
+                </div>
+            </section>
+
         </div>
     )
 }
