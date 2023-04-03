2b6b865e83b6c7b382e3338875de70585c2fc3c5
ZachLee12 <leezhengyang22@gmail.com>
2023-04-03 19:16:01 +0200

    Add responsive design for medium and small sized screen

--- src/Routes/About/About.jsx
+++ src/Routes/About/About.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import HikingImage from '../../assets/images/hiking.webp'
+import HikingImage2 from '../../assets/images/hiking2.jpg'
+import HikingImageSmall from '../../assets/images/hiking-small.jpg'
 
 export default function About() {
 
     return (
         <div id='About'>
-            <div className='about-us-content-wrapper'>
+            <div className="about-us-position-div small" id='below-1150px'>
+                <img className='about-us-image-above-1150px' id='below-1150px' src={HikingImage2} alt="" />
+                <img src={HikingImage} className="about-us-image-below-1150px" id='below-1150px' />
+            </div>
+            <div className='about-us-content-wrapper' id='below-1150px'>
 
                 <div className="about-us-content">
-                    <div className="about-us-float-container"></div>
-                    <p className='about-us-title'>About Us</p>
-                    <img className='about-us-image' src={HikingImage} alt="" />
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore ab cumque id, consequatur voluptatum, voluptatibus magnam unde expedita deserunt ipsa in. Eaque vero deleniti eligendi esse, odit sed nemo nisi.
-                    Dolorum provident pariatur, quas, nihil dolor, commodi consequatur adipisci non ab asperiores harum minus? Eligendi ut distinctio, totam alias doloribus cum voluptatem ipsum velit a incidunt commodi cumque minus omnis.
+                    <div className="about-us-float-container" id='below-1150px'></div>
+                    <img src={HikingImageSmall} className='about-us-image small' />
+
+                    <div className="about-us-text-content-wrapper" id='below-1150px' >
+                        <div className="text-content-float-container small" id='below-1150px'></div>
+                        <p className='about-us-title small'>About Us</p>
+                        <div className="about-us-text">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore ab cumque id, consequatur voluptatum, voluptatibus magnam unde expedita deserunt ipsa in. Eaque vero deleniti eligendi esse, odit sed nemo nisi.
+                            Dolorum provident pariatur, quas, nihil dolor, commodi consequatur adipisci non ab asperiores harum minus? Eligendi ut distinctio, totam alias doloribus cum voluptatem ipsum velit a incidunt commodi cumque minus omnis.
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
