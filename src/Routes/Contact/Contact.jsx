82f6ce60f403f6f3e1f4c08613785f6411420f9d
ZachLee12 <leezhengyang22@gmail.com>
2023-04-16 19:18:26 +0200

    Add contact background animation

--- src/Routes/Contact/Contact.jsx
+++ src/Routes/Contact/Contact.jsx
@@ -19,45 +19,46 @@ export default function Contact() {
 
     const googleMapsContainerStyle = {
         width: '100%',
-        height: '400px',
+        height: '100%',
     };
 
     return (
         <div id="Contact">
-
             <div className="contact-content">
-                <div className="google-maps">
-                    {isLoaded ? <GoogleMap
-                        mapContainerStyle={googleMapsContainerStyle}
-                        center={mockStoreLocation}
-                        zoom={10}
-                    >
-                    </GoogleMap> :
-                        <div>
-                            Google Maps should be here but I need money to buy Google Maps services
-                        </div>
-                    }
-                </div>
 
                 <div className="master-info-wrapper">
-                    <div className="contact-image"></div>
-                    <div className="linkedIn-info-wrapper info-wrapper">
-                        <img src={EmailIcon} className="icon" />
-                        <p className='info-paragraph'>
-                            Zach Lee
-                        </p>
+                    {/* <div className="contact-image"></div> */}
+                    <div className="google-maps">
+                        {isLoaded ? <GoogleMap
+                            mapContainerStyle={googleMapsContainerStyle}
+                            center={mockStoreLocation}
+                            zoom={10}
+                        >
+                        </GoogleMap> :
+                            <div>
+                                Google Maps should be here but I need money to buy Google Maps services
+                            </div>
+                        }
                     </div>
-                    <div className="location-info-wrapper info-wrapper">
-                        <img src={LocationIcon} className="icon" />
-                        <p className='info-paragraph'>
-                            Strasse, 6006 Kanton
-                        </p>
-                    </div>
-                    <div className="email-info-wrapper info-wrapper">
-                        <img src={EmailIcon} className="icon" />
-                        <p className='info-paragraph'>
-                            notmyemail@email.com
-                        </p>
+                    <div className="contact-info-wrapper">
+                        <div className="linkedIn-info-wrapper info-wrapper medium">
+                            <img src={EmailIcon} className="icon" />
+                            <p className='info-paragraph'>
+                                Zach Lee
+                            </p>
+                        </div>
+                        <div className="location-info-wrapper info-wrapper">
+                            <img src={LocationIcon} className="icon" />
+                            <p className='info-paragraph'>
+                                Strasse, 6006 Kanton
+                            </p>
+                        </div>
+                        <div className="email-info-wrapper info-wrapper">
+                            <img src={EmailIcon} className="icon" />
+                            <p className='info-paragraph'>
+                                notmyemail@email.com
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div >
